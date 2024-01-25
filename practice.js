6427e7603a59982072b4c66c753279b4b9339e8c
OlenaMyla <njonja1309@gmail.com>
2024-01-25 15:48:05 +0200

    primitives

---
+++ practice.js
@@ -0,0 +1,24 @@
+let age = 33;
+console.log(typeof 5);
+console.log(" Olenka " + age);
+console.log(typeof 'Olenka');
+console.log(typeof `Olenka ${age}`);
+
+
+console.log(typeof 1234567n);
+console.log(typeof BigInt(123));
+console.log(typeof Symbol("unique"));
+
+console.log(null);
+
+let variable;
+console.log(typeof variable);
+
+
+let thisYear = 2024;
+let nextYear = 2025;
+console.log(thisYear === nextYear)
+
+let thisYear1 = 2024;
+let thisYear2 = "2024";
+console.log(thisYear1 == thisYear2)
